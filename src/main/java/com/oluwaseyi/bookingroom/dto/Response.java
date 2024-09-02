67734ce4706335ff240b0744caaf2a187ee0833f
seyi-react <oluwaseyiadeoti825@gmail.com>
2024-09-02 10:26:52 +0100

    Response body structured

---
+++ src/main/java/com/oluwaseyi/bookingroom/dto/Response.java
@@ -0,0 +1,31 @@
+package com.oluwaseyi.bookingroom.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Response {
+
+    
+    private int code;
+    private String message;
+    private String token;
+    private String role;
+    private String expirationTime;
+    private String bookingCode;
+    private userDTO user;
+    private RoomDTO room;
+    private BookingDTO booking;
+    private List<userDTO> userList;
+    private  List<RoomDTO> roomList;
+    private List<BookingDTO> bookingList;
+
+}
